0617815e60851a5048efabcf59c92a999556f47b
abd elhafid <abdelhafidazligue162@gmail.com>
2025-11-11 09:28:04 +0100

    edited on renderStats function, added the form and it's validation except for the variants, also added a function that pushs event to their array

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // Data storing
 let events = [];
 let archive = [];
-// stats variables
-let totalEvents = events.length;
-let totalSeats = events.reduce((sum, e) => sum + e.seats, 0);
-let totalRevenue = events.reduce((sum,e) => sum + e.price * e.seats, 0);
 
 let sections = document.querySelectorAll("section");
 let btns = Array.from(document.querySelectorAll("button")); // from node list to an array
@@ -34,6 +30,7 @@ for(let i = 0; i < btns.length; i++){
             case 0:
                 sectionTitle.innerHTML = "Statistics";
                 sectionSubTitle.innerHTML = "Overview of your events";
+                renderStats();
                 break;
             case 1:
                 sectionTitle.innerHTML = "Add event";
@@ -48,14 +45,16 @@ for(let i = 0; i < btns.length; i++){
                 sectionSubTitle.innerHTML = "List of events you deleted in the past";
                 break;
         }
-        if(i == 0)
-            renderStats();
     })
 }
 
 // ======= stats ==========
 
 function renderStats(){
+    let totalEvents = events.length;
+    let totalSeats = events.reduce((sum, e) => sum + e.seats, 0);
+    let totalRevenue = events.reduce((sum,e) => sum + e.price * e.seats, 0);
+
     let statEvents = document.getElementById("stat-total-events");
     let statSeats = document.getElementById("stat-total-seats");
     let statPrice = document.getElementById("stat-total-price");
@@ -63,4 +62,58 @@ function renderStats(){
     statSeats.innerHTML = `${totalSeats}`;
     statPrice.innerHTML = `$${totalRevenue}`;
 }
-renderStats();
+
+// ======== add event form =============
+
+function handlFormSubmit(e){
+    e.preventDefault();
+    
+    let isValid = true;
+    titleRegex = /[a-zA-Z]{2,20}/;
+
+    // checking title
+    if(!(titleRegex.test(document.getElementById('event-title').value.trim()))){
+        isValid = false;
+        document.getElementById('event-title').classList.add("one-err");
+        console.error("the title doesn't respect the regex");
+    }
+    else{
+        console.log("title is correct");
+    }
+
+    // to check if all infos are correct
+    if(isValid){
+        let confirmed = confirm("Are you sure!");
+        if(confirmed){
+            moveToEvents();
+            console.log(confirmed);
+            document.getElementById('event-form').reset();
+            document.querySelector('#form-errors').className = "alert alert--error is-hidden";
+            document.querySelectorAll('.input').forEach(ele => {
+            ele.className = "input";
+            console.log(events);
+            console.log(events.length);
+        })
+        }
+    }
+    else{ // if the infos are not valid
+        document.querySelector('#form-errors').innerHTML = "Something is wrong!";
+        document.querySelector('#form-errors').className = "alert alert--error";
+        console.error("you form is undone");
+    }
+}
+
+document.getElementById('event-form').addEventListener('submit',handlFormSubmit);
+console.log("initially: ", events);
+
+// pushing event to the events array
+
+function moveToEvents(){
+    events.push({
+                title: document.getElementById('event-title').value,
+                imageUrl: document.getElementById('event-image').value,
+                description: document.getElementById('event-description').value,
+                seats: document.getElementById('event-seats').value,
+                price: document.getElementById('event-price').value,
+            })
+}
